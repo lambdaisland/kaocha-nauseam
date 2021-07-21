f426b6a1935a7b86c8a65aa7f965706b61d3af40
A Brooks <zab1000+github@gmail.com>
2021-07-21 13:40:09 -0500

    Initialize cljs-main

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+((nil . ((cider-clojure-cli-global-options     . "-A:dev:test")
+         (cider-default-cljs-repl              . browser)
+         (cider-preferred-build-tool           . clojure-cli)
+         (cider-redirect-server-output-to-repl . t)
+         (cider-repl-display-help-banner       . nil)
+         (clojure-toplevel-inside-comment-form . t))))
