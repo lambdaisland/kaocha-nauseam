a213ab11dcc2e4a7bb11bb48f8554461fa6821fd
Alys Brooks <alys@gaiwan.co>
2021-10-19 16:53:16 -0500

    Add nested.

---
+++ test/lambdaisland/simple_nested/nausem02_test.clj
@@ -0,0 +1,304 @@
+(ns lambdaisland.simple-nested.nausem02-test
+(:require [clojure.test :refer :all])
+)
+
+(deftest should-pass-0
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-1
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-2
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-3
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-4
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-5
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-6
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-7
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-8
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-9
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-10
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-11
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-12
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-13
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-14
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-15
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-16
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-17
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-18
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-19
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-20
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-21
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-22
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-23
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-24
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-25
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-26
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-27
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-28
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-29
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-30
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-31
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-32
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-33
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-34
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-35
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-36
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-37
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-38
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-39
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-40
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-41
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-42
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-43
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-44
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-45
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-46
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-47
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-48
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-49
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-50
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-51
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-52
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-53
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-54
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-55
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-56
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-57
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-58
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-59
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-60
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-61
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-62
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-63
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-64
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-65
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-66
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-67
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-68
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-69
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-70
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-71
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-72
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-73
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-74
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-75
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-76
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-77
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-78
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-79
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-80
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-81
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-82
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-83
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-84
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-85
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-86
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-87
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-88
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-89
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-90
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-91
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-92
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-93
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-94
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-95
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-96
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-97
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-98
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
+(deftest should-pass-99
+(testing "ridiculous test that should pass"
+  (is (do (Thread/sleep 1000) (= 1 1)))))
